d1d8a2f7e8cec9f80a18be1805e25932daa08320
HP <HP@LAPTOP-A9ET21V9>
2024-10-24 15:54:41 +0530

    This is my java basics project

---
+++ javabasics/src/core_javaprogram/Demo.java
@@ -0,0 +1,21 @@
+package core_javaprogram;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("java session started");
+//		System.out.print("by thamizhselvan");
+		int a,b,c;
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is:"+c);
+			}
+	}
+	
+
